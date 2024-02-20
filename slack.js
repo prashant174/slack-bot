6d1a043ea0dd66ff51f448a9d6cf36f58230ed7b
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-02-20 19:19:16 +0530

    Update slack.js

--- slack.js
+++ slack.js
@@ -1,18 +1,19 @@
 const { App } = require('@slack/bolt');
 const axios = require('axios');
 require('dotenv').config();
+
 const app = new App({
-  token:process.env.YOUR_SLACK_BOT_TOKEN,
-  signingSecret:process.env.YOUR_SLACK_SIGNING_SECRET,
+  token: process.env.YOUR_SLACK_BOT_TOKEN,
+  signingSecret: process.env.YOUR_SLACK_SIGNING_SECRET,
 });
-
-
 
 app.message(async ({ message, say }) => {
   const urlRegex = /(https?:\/\/[^\s]+)/;
   const match = message.text.match(urlRegex);
+  
   if (match) {
     const longUrl = match[0];
+
     try {
       const response = await axios.post('https://slack-url-shortner-by-prashant.onrender.com/shorten', { longUrl });
       await say(`Shortened URL: ${response.data.shortUrl}`);
@@ -24,6 +25,6 @@ app.message(async ({ message, say }) => {
 });
 
 (async () => {
-  await app.start(3000);
+  await app.start(process.env.PORT || 3000);
   console.log('Bot is running...');
-})(); 
+})();
